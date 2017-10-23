cb4f8ff8c65c1736ce89809ef59b80a0e439570f
Ben Lee <leefurong@gmail.com>
2017-10-23 13:56:07 +0800

    实现了category页面的控制部分（url, views）

--- website/mobile/urls.py
+++ website/mobile/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('article/<int:article_id>/', views.article, name='article'),
+    path('category/<int:category_id>/', views.category, name='category'),
 ]
--- website/mobile/views.py
+++ website/mobile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from core.models import Article, Category
 
 
@@ -21,4 +21,15 @@ def article(request, article_id):
     return render_to_response('mobile/article.html',
         {
             'article': article_obj
+        })
+
+
+
+def category(request, category_id):
+    category_obj = get_object_or_404(Category, pk=category_id)
+    articles = category_obj.article_set.all()
+    return render_to_response('mobile/category.html',
+        {
+            'category': category_obj.title,
+            'articles': articles,
         })
