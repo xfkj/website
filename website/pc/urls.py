2757455f7693c25b29105993c1b841ab1877633e
Ben Lee <leefurong@gmail.com>
2017-11-15 23:34:05 +0800

    修改了pc端的urls， path里的name加了pc_前缀。 否则会和移动端的url名字冲突。

--- website/pc/urls.py
+++ website/pc/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('article/<int:article_id>/', views.article, name='article'),
-    path('category/<int:category_id>/', views.category, name='category'),
-    path('article/<str:article_title>/', views.article, name='article_by_title'),
-    path('category/<str:category_title>/', views.category, name='category_by_title'),
+    path('', views.home, name='pc_home'),
+    path('article/<int:article_id>/', views.article, name='pc_article'),
+    path('category/<int:category_id>/', views.category, name='pc_category'),
+    path('article/<str:article_title>/', views.article, name='pc_article_by_title'),
+    path('category/<str:category_title>/', views.category, name='pc_category_by_title'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
