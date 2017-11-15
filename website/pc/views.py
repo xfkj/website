bb5a9c3a5b867608f8c7b54dcb9dc1d75b3f2cb0
maczyt <369280416@qq.com>
2017-11-15 19:54:58 +0800

    liujie:校园介绍

--- website/pc/views.py
+++ website/pc/views.py
@@ -50,6 +50,6 @@ def props(obj):
     pr = {}  
     for name in dir(obj):  
         value = getattr(obj, name)  
-        if not.name.startswith('__') and not callable(value):  
+        if not name.startswith('__') and not callable(value):
             pr[name] = value  
     return pr  
