c6c57ed0d3211602e659d4de2edf49e31b76c4e0
apple <apple@appledeMac-mini.local>
2017-12-26 22:36:00 +0800

    去掉wsgi多余代码

--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -10,20 +10,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import django.core.handlers.wsgi
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 application = get_wsgi_application()
-
-_application = django.core.handlers.wsgi.WSGIHandler()
-
-DOMAIN_NAME = 'www.ffjy.org'
-def application(environ, start_response):
-  if environ['HTTP_HOST'] != DOMAIN_NAME:
-    location = DOMAIN_NAME + environ['PATH_INFO']
-    if environ.get('QUERY_STRING'):
-      location += '?' + environ['QUERY_STRING']
-    start_response('301 Redirect', [('Location', location),])
-    return []
-  return _application(environ, start_response)
