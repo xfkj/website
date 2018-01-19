0da66ca4329ade8d035b80a8e22abe58a8c1dc8a
xfkj <it@ffjy.org>
2018-01-19 20:41:34 +0800

    完成画室大全网站api

--- website/core/urls.py
+++ website/core/urls.py
@@ -3,4 +3,9 @@ from core import views
 
 urlpatterns = [
     path('api/visitors', views.visitor),
+    path('studios', views.studios),
+    path('studios/index', views.studios_index),
+    path('news', views.news),
+    path('news/index', views.news_index),
+    path('articles/<int:id>', views.article),
 ]
--- website/core/views.py
+++ website/core/views.py
@@ -1,9 +1,10 @@
+import json
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from core.models import VisitorRecord, Article
+from django.http import HttpResponse, JsonResponse
+from core.models import VisitorRecord, Article, Category
 
 @csrf_exempt
 def visitor(request):
@@ -16,3 +17,21 @@ def visitor(request):
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=400, reason='only POST is accepted')
+
+def studios(request):
+    res = list(Article.objects.filter(category__title='画室').values('id', 'desc', 'cover', 'title'))
+    return JsonResponse(res, safe=False)
+def studios_index(request):
+    res = list(Article.objects.filter(category__title='画室', promote=True).values('id', 'desc', 'cover', 'title'))
+    return JsonResponse(res, safe=False)
+
+def news(request):
+    res = list(Article.objects.filter(category__title='非凡头条').values('id', 'desc', 'cover', 'title'))
+    return JsonResponse(res, safe=False)
+def news_index(request):
+    res = list(Article.objects.filter(category__title='非凡头条', promote=True).values('id', 'desc', 'cover', 'title'))
+    return JsonResponse(res, safe=False)
+
+def article(request, id):
+    res = list(Article.objects.filter(pk=id).values('cover', 'title', 'content', 'keyword', 'desc'))
+    return JsonResponse(res, safe=False)
