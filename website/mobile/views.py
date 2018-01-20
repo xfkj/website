0556878fce33127fca9cbdffc08f930e13c74601
xfkj <it@ffjy.org>
2018-01-20 16:30:24 +0800

    解决手机域名解析问题

--- website/mobile/views.py
+++ website/mobile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404, redirect
 from core.models import Article, Category
 
 def home(request):
@@ -49,9 +49,7 @@ def category(request, category_id=None, category_title=None):
         })
 
 def myfz(request):
-    return render_to_response('mobile/myfz.html')
-
-
+    return redirect('//m.myfz.ffjy.org')
 
 def get_template_for_category(category):
     cat_name = category.title
