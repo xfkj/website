e5e84ac6eb03cd017f8d9686ad8782c1147ae539
xfkj <it@ffjy.org>
2018-05-28 11:31:16 +0800

    恢复样式

--- website/core/admin.py
+++ website/core/admin.py
@@ -47,8 +47,6 @@ class VisitorAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
-    def has_delete_permission(self, request, obj=None):
-        return False
 
 
 admin.site.register(VisitorRecord, VisitorAdmin)
